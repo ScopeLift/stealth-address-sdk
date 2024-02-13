89bcc20fb5d852b732ee8b991d4d15d1c51dac01
marcomariscal <marco.a.mariscal@gmail.com>
2024-02-13 09:45:39 -0800

    feat: parse out adding priv key addition for testability

--- src/utils/crypto/computeStealthKey.ts
+++ src/utils/crypto/computeStealthKey.ts
@@ -23,12 +23,12 @@ function computeStealthKey({
 
   const spendingPrivateKeyBigInt = BigInt(spendingPrivateKey);
   const hashedSecretBigInt = BigInt(hashedSharedSecret);
-  const curveOrderBigInt = BigInt(`0x${CURVE.n.toString(16)}`);
 
-  // Compute the stealth private key by summing the spending private key and the hashed shared secret, then applying modulo with the curve's order.
-  // This ensures the resulting key is within the elliptic curve's valid scalar range.
-  const stealthPrivateKeyBigInt =
-    (spendingPrivateKeyBigInt + hashedSecretBigInt) % curveOrderBigInt;
+  // Compute the stealth private key by summing the spending private key and the hashed shared secret.
+  const stealthPrivateKeyBigInt = addPriv({
+    a: spendingPrivateKeyBigInt,
+    b: hashedSecretBigInt,
+  });
 
   const stealthPrivateKeyHex =
     `0x${stealthPrivateKeyBigInt.toString(16).padStart(64, '0')}` as HexString;
@@ -36,4 +36,11 @@ function computeStealthKey({
   return stealthPrivateKeyHex;
 }
 
+// Adds two private key scalars, ensuring the resulting key is within the elliptic curve's valid scalar range (modulo the curve's order).
+function addPriv({ a, b }: { a: bigint; b: bigint }) {
+  const curveOrderBigInt = BigInt(`0x${CURVE.n.toString(16)}`);
+  return (a + b) % curveOrderBigInt;
+}
+
+export { addPriv };
 export default computeStealthKey;
