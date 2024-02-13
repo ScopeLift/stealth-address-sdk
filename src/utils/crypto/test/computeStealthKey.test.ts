42ff87aa5ed2b41316eabf1429ec78d50c44c15d
marcomariscal <marco.a.mariscal@gmail.com>
2024-02-13 09:45:17 -0800

    feat: modulo math tests

--- src/utils/crypto/test/computeStealthKey.test.ts
+++ src/utils/crypto/test/computeStealthKey.test.ts
@@ -6,8 +6,9 @@ import {
   VALID_SCHEME_ID,
 } from '..';
 import { publicKeyToAddress } from 'viem/accounts';
-import { getPublicKey } from '@noble/secp256k1';
+import { getPublicKey, CURVE } from '@noble/secp256k1';
 import { bytesToHex, hexToBytes } from 'viem';
+import { addPriv } from '../computeStealthKey';
 
 describe('generateStealthAddress and computeStealthKey', () => {
   const schemeId = VALID_SCHEME_ID.SCHEME_ID_1;
@@ -47,5 +48,28 @@ describe('generateStealthAddress and computeStealthKey', () => {
     expect(generatedStealthAddressResult.stealthAddress).toEqual(
       computedStealthAddress
     );
+  });
+});
+
+describe('adding private keys', () => {
+  const privateKey1 = BigInt(
+    '0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DEDFE92F46681B20A0'
+  );
+  const privateKey2 = BigInt(
+    '0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DEDFE92F46681B20A0'
+  );
+  const curveOrder = BigInt(CURVE.n);
+
+  test('adding private keys without modulo should exceed the curve order', async () => {
+    const sumWithoutModulo = privateKey1 + privateKey2;
+    expect(sumWithoutModulo).toBeGreaterThan(curveOrder);
+  });
+
+  test('adding private keys with modulo should be within the curve order', async () => {
+    const sumWithModulo = addPriv({
+      a: privateKey1,
+      b: privateKey2,
+    });
+    expect(sumWithModulo).toBeLessThanOrEqual(curveOrder);
   });
 });
