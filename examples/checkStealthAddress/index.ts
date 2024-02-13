717f61da96e1546032bc9e11564847a7048d32d8
marcomariscal <marco.a.mariscal@gmail.com>
2024-02-13 12:37:19 -0800

    feat: example for checkStealthAddress

---
+++ examples/checkStealthAddress/index.ts
@@ -0,0 +1,36 @@
+import {
+  checkStealthAddress,
+  generateRandomStealthMetaAddress,
+  generateStealthAddress,
+  VALID_SCHEME_ID,
+} from 'stealth-address-sdk';
+import { hexToBytes } from 'viem';
+
+// User's keys (for example purposes, real values should be securely generated and stored)
+const {
+  stealthMetaAddressURI,
+  spendingPublicKey: userSpendingPublicKey,
+  viewingPrivateKey: userViewingPrivateKey,
+} = generateRandomStealthMetaAddress();
+
+// Generate a stealth address
+const { stealthAddress, ephemeralPublicKey, viewTag } = generateStealthAddress({
+  schemeId: VALID_SCHEME_ID.SCHEME_ID_1,
+  stealthMetaAddressURI,
+});
+
+console.log(`Stealth Address: ${stealthAddress}`);
+console.log(`Ephemeral Public Key: ${ephemeralPublicKey}`);
+console.log(`View Tag: ${viewTag}`);
+
+// Check if an announcement (simulated here) is for the user
+const isForUser = checkStealthAddress({
+  ephemeralPublicKey, // From the announcement
+  schemeId: VALID_SCHEME_ID.SCHEME_ID_1,
+  spendingPublicKey: hexToBytes(userSpendingPublicKey),
+  userStealthAddress: stealthAddress, // User's known stealth address
+  viewingPrivateKey: userViewingPrivateKey,
+  viewTag, // From the announcement
+});
+
+console.log(`Is the announcement for the user? ${isForUser}`);
