0c01463c85d025bdb8b190768709b239110092db
marcomariscal <marco.a.mariscal@gmail.com>
2024-02-15 09:23:41 -0800

    fix: setup

--- src/lib/actions/getAnnouncements/getAnnouncements.test.ts
+++ src/lib/actions/getAnnouncements/getAnnouncements.test.ts
@@ -1,6 +1,6 @@
 import { describe, test, expect } from 'bun:test';
 import { getAnnouncements } from '..';
-import ERC556AnnouncerAbi from '../../abi/ERC5564Announcer'
+import ERC556AnnouncerAbi from '../../abi/ERC5564Announcer';
 import {
   ANNOUNCER_CONTRACTS,
   VALID_SCHEME_ID,
@@ -17,8 +17,10 @@ describe('getAnnouncements', async () => {
   const stealthMetaAddressReceiver =
     'st:eth:0x020c828476b87a4d391f8b4b98de012125adb5be021a2dc6761cd6265219d901e203860c009a2c71de2a4abdf892804d52d56770599d6407bbdac77c311be98bd7e7';
 
-    const schemeId = (VALID_SCHEME_ID.SCHEME_ID_1;
+  const schemeId = VALID_SCHEME_ID.SCHEME_ID_1;
+  const ERC5564_CONTRACT_DEPLOY_BLOCK = BigInt(5281643);
 
+  // Setup
   const { stealthAddress, viewTag, ephemeralPublicKey } =
     generateStealthAddress({
       stealthMetaAddressURI: stealthMetaAddressReceiver,
@@ -34,28 +36,18 @@ describe('getAnnouncements', async () => {
     transport: http(process.env.SEPOLIA_RPC_URL!),
   });
 
-  const publicClient = createPublicClient({
-    chain: sepolia,
-    transport: http(process.env.SEPOLIA_RPC_URL!),
-  });
-
   console.log('account:', await walletClient.getAddresses());
   console.log('stealthAddress:', stealthAddress);
-
-
 
   // write to contract
   walletClient.writeContract({
     address: ANNOUNCER_CONTRACTS.get(11155111)!,
     functionName: 'announce',
     args: [BigInt(schemeId), stealthAddress, ephemeralPublicKey, viewTag],
-    abi: ERC556AnnouncerAbi
+    abi: ERC556AnnouncerAbi,
   });
-  })
 
   test('fetches announcements successfully', async () => {
-    // Generate stealth address
-    // Setup
     const rpcUrl = process.env.SEPOLIA_RPC_URL;
     const chainId = 11155111; // Sepolia
     const ERC5564Address = ANNOUNCER_CONTRACTS.get(chainId);
@@ -64,7 +56,7 @@ describe('getAnnouncements', async () => {
       clientParams: { rpcUrl: rpcUrl!, chainId },
       ERC5564Address: ERC5564Address!,
       args: {},
-      fromBlock: BigInt(5281643), // Contract deployment block
+      fromBlock: BigInt(ERC5564_CONTRACT_DEPLOY_BLOCK),
     });
 
     // Assert
