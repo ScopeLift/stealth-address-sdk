a435a6fbf56559c40878adfa1910ff89a55a1be2
marcomariscal <marco.a.mariscal@gmail.com>
2024-02-16 13:06:23 -0800

    feat: comments

--- src/lib/actions/getStealthMetaAddress/getStealthMetaAddress.ts
+++ src/lib/actions/getStealthMetaAddress/getStealthMetaAddress.ts
@@ -5,7 +5,21 @@ import type {
   GetStealthMetaAddressReturnType,
 } from './types';
 
-// Registrant may be a 160 bit address or other recipient identifier, such as an ENS name.
+/**
+ * Retrieves a stealth meta address from the ERC-6538 Registry contract.
+ *
+ * The `registrant` parameter can represent different types of recipient identifiers,
+ * including a standard Ethereum address (160-bit) or other formats like ENS names.
+ *
+ * @param {GetStealthMetaAddressParams} params - The parameters for fetching the stealth meta address.
+ *   - `clientParams`: (Optional if stealthClient is set up) Client parameters for stealthClient initialization.
+ *   - `ERC6538Address`: The address of the ERC-6538 Registry contract.
+ *   - `registrant`: The registrant identifier (Ethereum address or ENS name).
+ *   - `schemeId`: The ID of the stealth address scheme to use.
+ * @returns {Promise<GetStealthMetaAddressReturnType>} The stealth meta address.
+ *
+ * @throws {Error} If there is an error fetching the stealth meta address from the contract.
+ */
 async function getStealthMetaAddress({
   clientParams,
   ERC6538Address,
