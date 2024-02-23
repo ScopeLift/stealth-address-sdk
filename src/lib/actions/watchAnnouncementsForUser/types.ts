16d3a9ee4ba08d8ee2baf195f5d7eecbf5717ce2
marcomariscal <marco.a.mariscal@gmail.com>
2024-02-23 11:29:40 -0800

    feat: reuse type

--- src/lib/actions/watchAnnouncementsForUser/types.ts
+++ src/lib/actions/watchAnnouncementsForUser/types.ts
@@ -1,23 +1,14 @@
-import type { Log } from 'viem';
 import type { EthAddress } from '../../..';
-import type { ClientParams } from '../../stealthClient/types';
-import type { AnnouncementArgs } from '../getAnnouncements/types';
+import type {
+  AnnouncementArgs,
+  AnnouncementLog,
+} from '../getAnnouncements/types';
+import type { GetAnnouncementsForUserParams } from '..';
 
 export type WatchAnnouncementsForUserParams<T> = {
-  clientParams?: ClientParams;
   ERC5564Address: EthAddress;
   args: AnnouncementArgs;
-  handleLogs: (logs: WatchAnnouncementLog[]) => T;
-};
-
-export type WatchAnnouncementLog = Log & {
-  args: {
-    caller: EthAddress;
-    ephemeralPubKey: `0x${string}`;
-    metadata: `0x${string}`;
-    schemeId: bigint;
-    stealthAddress: EthAddress;
-  };
-};
+  handleLogsForUser: (logs: AnnouncementLog[]) => T;
+} & GetAnnouncementsForUserParams;
 
 export type WatchAnnouncementsForUserReturnType = {};
