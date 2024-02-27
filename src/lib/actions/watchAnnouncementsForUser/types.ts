7defa425de9ca94bc2be81097426d77d28d5626a
marcomariscal <marco.a.mariscal@gmail.com>
2024-02-27 12:30:22 -0800

    feat: comments

--- src/lib/actions/watchAnnouncementsForUser/types.ts
+++ src/lib/actions/watchAnnouncementsForUser/types.ts
@@ -13,9 +13,13 @@ import type {
 export type WatchAnnouncementsForUserPollingOptions = GetPollOptions<Transport>;
 
 export type WatchAnnouncementsForUserParams<T> = {
+  /** The address of the ERC5564 contract. */
   ERC5564Address: EthAddress;
+  /** The arguments to filter the announcements. */
   args: AnnouncementArgs;
+  /** The callback function to handle the filtered announcement logs. */
   handleLogsForUser: (logs: AnnouncementLog[]) => T;
+  /** Optional polling options  */
   pollOptions?: WatchAnnouncementsForUserPollingOptions;
 } & Omit<GetAnnouncementsForUserParams, 'announcements'>;
 
--- src/lib/actions/watchAnnouncementsForUser/watchAnnouncementsForUser.ts
+++ src/lib/actions/watchAnnouncementsForUser/watchAnnouncementsForUser.ts
@@ -1,7 +1,23 @@
-import type { WatchAnnouncementsForUserParams } from '..';
+import type {
+  WatchAnnouncementsForUserParams,
+  WatchAnnouncementsForUserReturnType,
+} from '..';
 import { handleViemPublicClient } from '../../stealthClient/createStealthClient';
 import { ERC5564AnnouncerAbi, getAnnouncementsForUser } from '../..';
 
+/**
+ * Watches for announcement events relevant to the user.
+ *
+ * @template T - The return type of the handleLogsForUser callback function.
+ * @property {EthAddress} ERC5564Address - The Ethereum address of the ERC5564 contract.
+ * @property {AnnouncementArgs} args - Arguments to filter the announcements.
+ * @property {(logs: AnnouncementLog[]) => T} handleLogsForUser - Callback function to handle the filtered announcement logs.
+ *   This function receives an array of AnnouncementLog and returns a generic value.
+ * @property {WatchAnnouncementsForUserPollingOptions} [pollOptions] - Optional polling options to configure the behavior of watching announcements.
+ *   This includes configurations such as polling frequency.
+ * @property {Omit<GetAnnouncementsForUserParams, 'announcements'>} - Inherits all properties from GetAnnouncementsForUserParams except 'announcements'.
+ *   This typically includes cryptographic keys and filter lists for inclusion or exclusion of specific announcements.
+ */
 async function watchAnnouncementsForUser<T = void>({
   args,
   spendingPublicKey,
@@ -12,7 +28,7 @@ async function watchAnnouncementsForUser<T = void>({
   includeList,
   handleLogsForUser,
   pollOptions,
-}: WatchAnnouncementsForUserParams<T>) {
+}: WatchAnnouncementsForUserParams<T>): Promise<WatchAnnouncementsForUserReturnType> {
   const publicClient = handleViemPublicClient(clientParams);
 
   const unwatch = publicClient.watchContractEvent({
