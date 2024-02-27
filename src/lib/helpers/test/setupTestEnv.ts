6dc52debb6a21dd547ff788e2598d4268a80fd02
marcomariscal <marco.a.mariscal@gmail.com>
2024-02-27 11:26:29 -0800

    feat: setup local node testing

--- src/lib/helpers/test/setupTestEnv.ts
+++ src/lib/helpers/test/setupTestEnv.ts
@@ -2,17 +2,20 @@ import { createStealthClient } from '../..';
 import { ERC5564_CONTRACT, ERC6538_CONTRACT } from '../../..';
 import type { VALID_CHAIN_IDS } from '../types';
 
-function setupTestEnv() {
+/**
+ * Initializes a test environment testing purposes.
+ * @param {boolean} useLocal - Flag to determine if the local or remote RPC URL should be used.
+ * To use a local RPC URL, set the TEST_LOCAL_NODE_RPC_URL environment variable and use (for example)
+ * anvil to fork your rpc url. To use a remote RPC URL, set the TEST_RPC_URL environment variable and set useLocal to false.
+ * @returns An object containing the testing environment setup parameters including chain ID, contract addresses, and a stealth client instance.
+ */
+function setupTestEnv(useLocal: boolean = true) {
   if (!process.env.TEST_CHAIN_ID) {
     throw new Error('TEST_CHAIN_ID is not defined');
   }
   // Setup stealth client
   const chainId = getValidChainId(Number(process.env.TEST_CHAIN_ID));
-
-  const rpcUrl = process.env.TEST_RPC_URL;
-  if (!rpcUrl) {
-    throw new Error('TEST_RPC_URL is not defined');
-  }
+  const rpcUrl = getRpcUrl(useLocal);
   const stealthClient = createStealthClient({ rpcUrl, chainId });
 
   // Setup ERC5564 contract details
@@ -39,7 +42,12 @@ function setupTestEnv() {
   };
 }
 
-// Function to check if the given chain ID is valid and return it as VALID_CHAIN_IDS
+/**
+ * Validates the provided chain ID against a list of valid chain IDs.
+ * @param {number} chainId - The chain ID to validate.
+ * @returns {VALID_CHAIN_IDS} - The validated chain ID.
+ * @throws {Error} If the chain ID is not valid.
+ */
 function getValidChainId(chainId: number): VALID_CHAIN_IDS {
   if (chainId === 11155111 || chainId === 1) {
     return chainId as VALID_CHAIN_IDS;
@@ -47,5 +55,25 @@ function getValidChainId(chainId: number): VALID_CHAIN_IDS {
   throw new Error(`Invalid chain ID: ${chainId}`);
 }
 
-export { getValidChainId };
+/**
+ * Retrieves the RPC URL based on the `useLocal` flag.
+ * @param {boolean} useLocal - Determines which environment variable to use for the RPC URL.
+ * @returns {string} The RPC URL.
+ * @throws {Error} If the corresponding environment variable is not defined.
+ */
+function getRpcUrl(useLocal = false) {
+  const rpcUrl = useLocal
+    ? process.env.TEST_LOCAL_NODE_RPC_URL
+    : process.env.TEST_RPC_URL;
+  if (!rpcUrl) {
+    throw new Error(
+      useLocal
+        ? 'TEST_LOCAL_NODE_RPC_URL is not defined'
+        : 'TEST_RPC_URL is not defined'
+    );
+  }
+  return rpcUrl;
+}
+
+export { getValidChainId, getRpcUrl };
 export default setupTestEnv;
--- src/lib/helpers/test/setupTestWallet.ts
+++ src/lib/helpers/test/setupTestWallet.ts
@@ -1,34 +1,23 @@
 import { createWalletClient, http, publicActions } from 'viem';
-import { getChain } from '../chains';
+import { getChain as _getChain } from '../chains';
 import type { SuperWalletClient } from '../types';
 import { privateKeyToAccount } from 'viem/accounts';
-import { getValidChainId } from './setupTestEnv';
+import { getRpcUrl, getValidChainId } from './setupTestEnv';
 
-const setupTestWallet = (): SuperWalletClient => {
-  const chainIdEnv = process.env.TEST_CHAIN_ID;
-  if (!chainIdEnv) {
-    throw new Error('TEST_CHAIN_ID is not defined');
-  }
-  const chainId = getValidChainId(Number(chainIdEnv));
-
-  const privKey = process.env.TEST_PRIVATE_KEY;
-  if (!privKey) {
-    throw new Error('TEST_PRIVATE_KEY is not defined');
-  }
-
-  const account = privateKeyToAccount(
-    process.env.TEST_PRIVATE_KEY as `0x${string}`
-  );
-
-  const rpcUrl = process.env.TEST_RPC_URL;
-  if (!rpcUrl) {
-    throw new Error('TEST_RPC_URL is not defined');
-  }
-
-  const chain = getChain(chainId);
-  if (!chain) {
-    new Error('Chain not found');
-  }
+/**
+ * Initializes and configures a wallet client for testing purposes.
+ * @param {boolean} useLocal - Flag to determine if a local or remote RPC URL should be used.
+ * To use a local RPC URL, set the TEST_LOCAL_NODE_RPC_URL environment variable and use (for example)
+ * anvil to fork your rpc url. To use a remote RPC URL, set the TEST_RPC_URL environment variable and set useLocal to false.
+ * @returns {SuperWalletClient} A configured wallet client.
+ */
+const setupTestWallet = (useLocal: boolean = true): SuperWalletClient => {
+  // Retrieve the account from the private key set in environment variables.
+  const account = getAccount();
+  // Determine the RPC URL based on whether local or remote env is used.
+  const rpcUrl = getRpcUrl(useLocal);
+  // Obtain the viem chain information
+  const chain = getChain();
 
   const walletClient = createWalletClient({
     account,
@@ -41,6 +30,24 @@ const setupTestWallet = (): SuperWalletClient => {
   }
 
   return walletClient.extend(publicActions);
+};
+
+const getChain = () => {
+  const chainIdEnv = process.env.TEST_CHAIN_ID as string | undefined;
+  if (!chainIdEnv) {
+    throw new Error('TEST_CHAIN_ID is not defined');
+  }
+  const chainId = getValidChainId(Number(chainIdEnv));
+  return _getChain(chainId);
+};
+
+const getAccount = () => {
+  const privKey = process.env.TEST_PRIVATE_KEY as `0x${string} ` | undefined;
+  if (!privKey) {
+    throw new Error('TEST_PRIVATE_KEY is not defined');
+  }
+
+  return privateKeyToAccount(privKey);
 };
 
 export default setupTestWallet;
