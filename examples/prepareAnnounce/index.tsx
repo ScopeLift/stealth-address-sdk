77c6e0d72cf3f4613192c56d42c28a370f056d60
marcomariscal <marco.a.mariscal@gmail.com>
2024-02-29 12:04:25 -0800

    feat: comment

--- examples/prepareAnnounce/index.tsx
+++ examples/prepareAnnounce/index.tsx
@@ -11,23 +11,32 @@ import {
   VALID_SCHEME_ID,
 } from 'stealth-address-sdk';
 
-// Initialize your environment variables or configuration
-const chainId = 11155111; // Example chain ID
-const rpcUrl = import.meta.env.VITE_RPC_URL!; // Your Ethereum RPC URL
-// Example  URI; see the getStealthMetaAddress example and generateRandomStealthMetaAddress helper for more details
-const stealthMetaAddressURI = import.meta.env.VITE_STEALTH_META_ADDRESS_URI!;
-const chain = sepolia; // Example Viem chain
-
-// Initialize Viem wallet client if using Viem
-const walletClient = createWalletClient({
-  chain,
-  transport: custom(window.ethereum!),
-});
-
-// Initialize the stealth client with your RPC URL and chain ID
-const stealthClient = createStealthClient({ rpcUrl, chainId });
-
+/**
+ * This React component demonstrates the process of connecting to a wallet and announcing a stealth address.
+ * It utilizes Viem's walletClient for wallet interaction and the stealth-address-sdk for stealth address operations.
+ *
+ * @returns The component renders a button to connect the wallet and announce the stealth address.
+ *
+ * @example
+ * To run this example, ensure you have set up your environment variables VITE_RPC_URL and VITE_STEALTH_META_ADDRESS_URI.
+ * Run the development server using Vite, `vite run dev`.
+ */
 const Example = () => {
+  // Initialize your environment variables or configuration
+  const chainId = 11155111; // Example chain ID
+  const rpcUrl = import.meta.env.VITE_RPC_URL!; // Your Ethereum RPC URL
+  // Example URI; see the getStealthMetaAddress example and generateRandomStealthMetaAddress helper for more details
+  const stealthMetaAddressURI = import.meta.env.VITE_STEALTH_META_ADDRESS_URI!;
+  const chain = sepolia; // Example Viem chain
+
+  // Initialize Viem wallet client if using Viem
+  const walletClient = createWalletClient({
+    chain,
+    transport: custom(window.ethereum!),
+  });
+
+  // Initialize the stealth client with your RPC URL and chain ID
+  const stealthClient = createStealthClient({ rpcUrl, chainId });
   const [account, setAccount] = useState<Address>();
 
   const connect = async () => {
