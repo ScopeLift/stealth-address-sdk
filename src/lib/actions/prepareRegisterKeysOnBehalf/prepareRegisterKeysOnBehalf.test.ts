86acf4e0663a2081f05c26816e716690617afa09
marcomariscal <marco.a.mariscal@gmail.com>
2024-03-01 16:20:43 -0800

    feat: set up signature (WIP)

--- src/lib/actions/prepareRegisterKeysOnBehalf/prepareRegisterKeysOnBehalf.test.ts
+++ src/lib/actions/prepareRegisterKeysOnBehalf/prepareRegisterKeysOnBehalf.test.ts
@@ -1,13 +1,17 @@
 import { describe, test, expect } from 'bun:test';
 import setupTestEnv from '../../helpers/test/setupTestEnv';
 import setupTestWallet from '../../helpers/test/setupTestWallet';
-import { VALID_SCHEME_ID, parseStealthMetaAddressURI } from '../../..';
+import {
+  ERC6538RegistryAbi,
+  VALID_SCHEME_ID,
+  parseStealthMetaAddressURI,
+} from '../../..';
 import setupTestStealthKeys from '../../helpers/test/setupTestStealthKeys';
 import type { RegisterKeysOnBehalfArgs } from './types';
 
 describe('prepareRegisterKeysOnBehalf', async () => {
-  const { stealthClient, ERC6538Address } = setupTestEnv();
-  const walletClient = setupTestWallet();
+  const { stealthClient, ERC6538Address, chainId } = setupTestEnv(false);
+  const walletClient = setupTestWallet(false);
   const { stealthMetaAddressURI } = setupTestStealthKeys();
   const schemeId = VALID_SCHEME_ID.SCHEME_ID_1;
   const stealthMetaAddressToRegister = parseStealthMetaAddressURI({
@@ -16,11 +20,53 @@ describe('prepareRegisterKeysOnBehalf', async () => {
   });
   const account = walletClient.account?.address!;
 
+  // Get the registrant's current nonce for the signature
+  const nonce = await walletClient.readContract({
+    address: ERC6538Address,
+    abi: ERC6538RegistryAbi,
+    functionName: 'nonceOf',
+    args: [account],
+  });
+
+  // Prepare the signature domain
+  const domain = {
+    name: 'ERC6538Registry',
+    version: '1.0',
+    chainId,
+    verifyingContract: ERC6538Address,
+  } as const;
+
+  // Taken from the ERC6538Registry contract
+  const primaryType = 'Erc6538RegistryEntry';
+
+  // Prepare the signature types
+  const types = {
+    [primaryType]: [
+      { name: 'schemeId', type: 'uint256' },
+      { name: 'stealthMetaAddress', type: 'bytes' },
+      { name: 'nonce', type: 'uint256' },
+    ],
+  } as const;
+
+  const message = {
+    schemeId: BigInt(schemeId),
+    stealthMetaAddress: stealthMetaAddressToRegister,
+    nonce,
+  };
+
+  const signature = await walletClient.signTypedData({
+    account: walletClient.account!, // Without this, no signer is available supposedly; TODO figure out why
+    primaryType,
+    domain,
+    types,
+    message,
+  });
+
   const args: RegisterKeysOnBehalfArgs = {
     registrant: account,
     schemeId,
     stealthMetaAddress: stealthMetaAddressToRegister,
-    signature: '0x1234567890', // TODO This is a placeholder for the signature
+    signature,
   };
 
   const prepared = await stealthClient.prepareRegisterKeysOnBehalf({
