f3214d77c5934629ecc07b2a8ee7dc4655846275
marcomariscal <marco.a.mariscal@gmail.com>
2024-03-05 13:10:10 -0800

    feat: split into own func

--- src/lib/actions/prepareRegisterKeysOnBehalf/prepareRegisterKeysOnBehalf.test.ts
+++ src/lib/actions/prepareRegisterKeysOnBehalf/prepareRegisterKeysOnBehalf.test.ts
@@ -20,61 +20,64 @@ describe('prepareRegisterKeysOnBehalf', async () => {
   });
   const account = walletClient.account?.address!;
 
-  // Get the registrant's current nonce for the signature
-  const nonce = await walletClient.readContract({
-    address: ERC6538Address,
-    abi: ERC6538RegistryAbi,
-    functionName: 'nonceOf',
-    args: [account],
-  });
+  const generateSignature = async () => {
+    // Get the registrant's current nonce for the signature
+    const nonce = await walletClient.readContract({
+      address: ERC6538Address,
+      abi: ERC6538RegistryAbi,
+      functionName: 'nonceOf',
+      args: [account],
+    });
 
-  // Prepare the signature domain
-  const domain = {
-    name: 'ERC6538Registry',
-    version: '1.0',
-    chainId,
-    verifyingContract: ERC6538Address,
-  } as const;
+    // Prepare the signature domain
+    const domain = {
+      name: 'ERC6538Registry',
+      version: '1.0',
+      chainId,
+      verifyingContract: ERC6538Address,
+    } as const;
 
-  // Taken from the ERC6538Registry contract
-  const primaryType = 'Erc6538RegistryEntry';
+    // Taken from the ERC6538Registry contract
+    const primaryType = 'Erc6538RegistryEntry';
+    const ERC6538REGISTRY_ENTRY_TYPE_HASH = await walletClient.readContract({
+      address: ERC6538Address,
+      abi: ERC6538RegistryAbi,
+      functionName: 'ERC6538REGISTRY_ENTRY_TYPE_HASH',
+    });
 
-  const ERC6538REGISTRY_ENTRY_TYPE_HASH = await walletClient.readContract({
-    address: ERC6538Address,
-    abi: ERC6538RegistryAbi,
-    functionName: 'ERC6538REGISTRY_ENTRY_TYPE_HASH',
-  });
+    // Prepare the signature types
+    const types = {
+      [primaryType]: [
+        { name: 'ERC6538REGISTRY_ENTRY_TYPE_HASH', type: 'bytes32' },
+        { name: 'schemeId', type: 'uint256' },
+        { name: 'stealthMetaAddress', type: 'bytes' },
+        { name: 'nonce', type: 'uint256' },
+      ],
+    } as const;
 
-  // Prepare the signature types
-  const types = {
-    [primaryType]: [
-      { name: 'ERC6538REGISTRY_ENTRY_TYPE_HASH', type: 'bytes32' },
-      { name: 'schemeId', type: 'uint256' },
-      { name: 'stealthMetaAddress', type: 'bytes' },
-      { name: 'nonce', type: 'uint256' },
-    ],
-  } as const;
+    const message = {
+      ERC6538REGISTRY_ENTRY_TYPE_HASH,
+      schemeId: BigInt(schemeId),
+      stealthMetaAddress: stealthMetaAddressToRegister,
+      nonce,
+    };
 
-  const message = {
-    ERC6538REGISTRY_ENTRY_TYPE_HASH,
-    schemeId: BigInt(schemeId),
-    stealthMetaAddress: stealthMetaAddressToRegister,
-    nonce,
+    const signature = await walletClient.signTypedData({
+      account: walletClient.account!, // Without this, no signer is available supposedly; TODO figure out why
+      primaryType,
+      domain,
+      types,
+      message,
+    });
+
+    return signature;
   };
-
-  const signature = await walletClient.signTypedData({
-    account: walletClient.account!, // Without this, no signer is available supposedly; TODO figure out why
-    primaryType,
-    domain,
-    types,
-    message,
-  });
 
   const args: RegisterKeysOnBehalfArgs = {
     registrant: account,
     schemeId,
     stealthMetaAddress: stealthMetaAddressToRegister,
-    signature,
+    signature: await generateSignature(),
   };
 
   const prepared = await stealthClient.prepareRegisterKeysOnBehalf({
