d5b2ae4243fdc992732a820c0accf509600f3d2d
marcomariscal <marco.a.mariscal@gmail.com>
2024-05-01 14:03:22 -0700

    chore: format

--- examples/checkStealthAddress/index.ts
+++ examples/checkStealthAddress/index.ts
@@ -1,21 +1,21 @@
 import {
-	checkStealthAddress,
-	generateRandomStealthMetaAddress,
-	generateStealthAddress,
-	VALID_SCHEME_ID,
-} from "@scopelift/stealth-address-sdk";
+  checkStealthAddress,
+  generateRandomStealthMetaAddress,
+  generateStealthAddress,
+  VALID_SCHEME_ID,
+} from '@scopelift/stealth-address-sdk';
 
 // User's keys (for example purposes, real values should be securely generated and stored)
 const {
-	stealthMetaAddressURI,
-	spendingPublicKey: userSpendingPublicKey,
-	viewingPrivateKey: userViewingPrivateKey,
+  stealthMetaAddressURI,
+  spendingPublicKey: userSpendingPublicKey,
+  viewingPrivateKey: userViewingPrivateKey,
 } = generateRandomStealthMetaAddress();
 
 // Generate a stealth address
 const { stealthAddress, ephemeralPublicKey, viewTag } = generateStealthAddress({
-	schemeId: VALID_SCHEME_ID.SCHEME_ID_1,
-	stealthMetaAddressURI,
+  schemeId: VALID_SCHEME_ID.SCHEME_ID_1,
+  stealthMetaAddressURI,
 });
 
 console.log(`Stealth Address: ${stealthAddress}`);
@@ -24,12 +24,12 @@ console.log(`View Tag: ${viewTag}`);
 
 // Check if an announcement (simulated here) is for the user
 const isForUser = checkStealthAddress({
-	ephemeralPublicKey, // From the announcement
-	schemeId: VALID_SCHEME_ID.SCHEME_ID_1,
-	spendingPublicKey: userSpendingPublicKey,
-	userStealthAddress: stealthAddress, // User's known stealth address
-	viewingPrivateKey: userViewingPrivateKey,
-	viewTag, // From the announcement
+  ephemeralPublicKey, // From the announcement
+  schemeId: VALID_SCHEME_ID.SCHEME_ID_1,
+  spendingPublicKey: userSpendingPublicKey,
+  userStealthAddress: stealthAddress, // User's known stealth address
+  viewingPrivateKey: userViewingPrivateKey,
+  viewTag, // From the announcement
 });
 
 console.log(`Is the announcement for the user? ${isForUser}`);
--- examples/generateStealthAddress/index.ts
+++ examples/generateStealthAddress/index.ts
@@ -1,6 +1,6 @@
-import { generateStealthAddress } from "@scopelift/stealth-address-sdk";
+import { generateStealthAddress } from '@scopelift/stealth-address-sdk';
 
-const stealthMetaAddressURI = "your_stealth_meta_address_here";
+const stealthMetaAddressURI = 'your_stealth_meta_address_here';
 const result = generateStealthAddress({ stealthMetaAddressURI });
 
 export default result;
