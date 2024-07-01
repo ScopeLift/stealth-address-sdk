6187916acf716c1552056f69054ee3bc6ebe01a5
marcomariscal <marco.a.mariscal@gmail.com>
2024-07-01 14:41:03 -0700

    chore: log

--- src/utils/helpers/generateKeysFromSignature.ts
+++ src/utils/helpers/generateKeysFromSignature.ts
@@ -16,13 +16,9 @@ function generateKeysFromSignature(signature: HexString): {
   if (!isValidSignature(signature)) {
     throw new Error(`Invalid signature: ${signature}`);
   }
-  console.log('🦄 ~ generateKeysFromSignature ~ signature:', signature);
 
   // Extract signature portions
   const { portion1, portion2, lastByte } = extractPortions(signature);
-  console.log('🦄 ~ generateKeysFromSignature ~ portion1:', portion1);
-  console.log('🦄 ~ generateKeysFromSignature ~ portion2:', portion2);
-  console.log('🦄 ~ generateKeysFromSignature ~ lastByte:', lastByte);
 
   if (`0x${portion1}${portion2}${lastByte}` !== signature) {
     throw new Error('Signature incorrectly generated or parsed');
