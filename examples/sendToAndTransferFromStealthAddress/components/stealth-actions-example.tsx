64d049ccea0a908c3b878baca7f81d1a60878f5b
marcomariscal <marco.a.mariscal@gmail.com>
2024-07-18 09:19:44 -0700

    fix: enum name

--- examples/sendToAndTransferFromStealthAddress/components/stealth-actions-example.tsx
+++ examples/sendToAndTransferFromStealthAddress/components/stealth-actions-example.tsx
@@ -50,7 +50,7 @@ const StealthActionsExample = () => {
     useState<`0x${string}`>();
   const [error, setError] = useState<string | null>(null);
 
-  enum CURRENT_STEP {
+  enum CurrentStep {
     GENERATE_STEALTH_ADDRESS = 1,
     SEND_TO_STEALTH_ADDRESS = 2,
     TRANSFER_FROM_STEALTH_ADDRESS = 3,
@@ -58,7 +58,7 @@ const StealthActionsExample = () => {
   }
 
   const [currentStep, setCurrentStep] = useState(
-    CURRENT_STEP.GENERATE_STEALTH_ADDRESS
+    CurrentStep.GENERATE_STEALTH_ADDRESS
   );
 
   const {
@@ -90,7 +90,7 @@ const StealthActionsExample = () => {
     try {
       const [address] = await walletClient.requestAddresses();
       setConnectedAccount(address);
-      setCurrentStep(CURRENT_STEP.GENERATE_STEALTH_ADDRESS);
+      setCurrentStep(CurrentStep.GENERATE_STEALTH_ADDRESS);
     } catch (error) {
       handleError('Error connecting wallet');
     }
@@ -116,7 +116,7 @@ const StealthActionsExample = () => {
         schemeId: SCHEME_ID
       });
       setStealthAddressDetails(details);
-      setCurrentStep(CURRENT_STEP.SEND_TO_STEALTH_ADDRESS);
+      setCurrentStep(CurrentStep.SEND_TO_STEALTH_ADDRESS);
     } catch (error) {
       handleError('Error generating stealth address');
     }
@@ -133,7 +133,7 @@ const StealthActionsExample = () => {
         value: parseEther(SEND_AMOUNT)
       });
       setSendToStealthTxHash(hash);
-      setCurrentStep(CURRENT_STEP.TRANSFER_FROM_STEALTH_ADDRESS);
+      setCurrentStep(CurrentStep.TRANSFER_FROM_STEALTH_ADDRESS);
     } catch (error) {
       handleError('Error sending to stealth address');
     }
@@ -163,7 +163,7 @@ const StealthActionsExample = () => {
         value: parseEther(WITHDRAW_AMOUNT)
       });
       setTransferFromStealthTxHash(hash);
-      setCurrentStep(CURRENT_STEP.TRANSFER_FROM_STEALTH_ADDRESS_COMPLETE);
+      setCurrentStep(CurrentStep.TRANSFER_FROM_STEALTH_ADDRESS_COMPLETE);
     } catch (error) {
       handleError('Error transferring from stealth address');
     }
@@ -172,7 +172,7 @@ const StealthActionsExample = () => {
   const handleError = (msg: string) => {
     console.error(msg);
     setError(msg);
-    setCurrentStep(CURRENT_STEP.GENERATE_STEALTH_ADDRESS);
+    setCurrentStep(CurrentStep.GENERATE_STEALTH_ADDRESS);
   };
 
   useEffect(() => {
