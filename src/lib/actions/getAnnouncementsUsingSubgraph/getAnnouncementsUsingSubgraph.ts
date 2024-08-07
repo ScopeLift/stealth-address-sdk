c38508fd11282c990cc0c91713caea3cfa2374be
marcomariscal <marco.a.mariscal@gmail.com>
2024-08-07 09:46:24 -0700

    feat: docs

--- src/lib/actions/getAnnouncementsUsingSubgraph/getAnnouncementsUsingSubgraph.ts
+++ src/lib/actions/getAnnouncementsUsingSubgraph/getAnnouncementsUsingSubgraph.ts
@@ -1,19 +1,43 @@
-import { GraphQLClient } from 'graphql-request';
-import type { AnnouncementLog } from '../getAnnouncements/types';
+import { GraphQLClient } from "graphql-request";
+import type { AnnouncementLog } from "../getAnnouncements/types";
 import {
   convertSubgraphEntityToAnnouncementLog,
-  fetchPages
-} from './subgraphHelpers';
+  fetchPages,
+} from "./subgraphHelpers";
 import type {
   GetAnnouncementsUsingSubgraphParams,
   GetAnnouncementsUsingSubgraphReturnType,
-  SubgraphAnnouncementEntity
-} from './types';
+  SubgraphAnnouncementEntity,
+} from "./types";
 
+/**
+ * Fetches announcement data from a specified subgraph URL.
+ *
+ * Supports filtering results based on a filter string that looks like:
+ *   "
+ *    blockNumber_gte: 123456789
+ *    caller: "0x123456789"
+ *   "
+ *
+ * `pageSize` can also be adjusted to optimize performance and/or align with the subgraph's pagination limits.
+ * The default value is 1000, which is a common maximum allowed by the subgraph providers.
+ * If the subgraph provider has a higher limit, it can be adjusted accordingly.
+ *
+ * @param {Object} params - The parameters for the function.
+ * @param {string} params.subgraphUrl - The URL of the subgraph to query.
+ * @param {string} [params.filter=''] - Optional filter string for the query.
+ * @param {number} [params.pageSize=1000] - Number of items to fetch per page.
+ *
+ * @returns {Promise<AnnouncementLog[]>} A promise that resolves to an array of AnnouncementLog objects.
+ * The return value here is the same as the `getAnnouncements` function to be able to seamlessly fallback to fetching via logs
+ * if the subgraph is not available.
+ *
+ * @throws {Error} If there's an issue fetching the announcements.
+ */
 async function getAnnouncementsUsingSubgraph({
   subgraphUrl,
-  filter = '',
-  pageSize = 1000
+  filter = "",
+  pageSize = 1000,
 }: GetAnnouncementsUsingSubgraphParams): Promise<GetAnnouncementsUsingSubgraphReturnType> {
   const client = new GraphQLClient(subgraphUrl);
   const gqlQuery = `
@@ -50,14 +74,14 @@ async function getAnnouncementsUsingSubgraph({
       gqlQuery,
       pageSize,
       filter,
-      entity: 'announcements'
+      entity: "announcements",
     })) {
       allAnnouncements.push(
         ...batch.map(convertSubgraphEntityToAnnouncementLog)
       );
     }
   } catch (error) {
-    console.error('Failed to fetch announcements:', error);
+    console.error("Failed to fetch announcements:", error);
     throw error;
   }
 
